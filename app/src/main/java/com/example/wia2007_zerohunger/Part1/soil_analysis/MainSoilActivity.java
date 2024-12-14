03c0af44f48ff777bb172f92110b6bff097e4d22
OIZHENHAU <zhenhau8072@gmail.com>
2024-12-14 18:25:05 +0800

    Add Soil Activity

--- app/src/main/java/com/example/wia2007_zerohunger/Part1/soil_analysis/MainSoilActivity.java
+++ app/src/main/java/com/example/wia2007_zerohunger/Part1/soil_analysis/MainSoilActivity.java
@@ -1,5 +1,6 @@
 package com.example.wia2007_zerohunger.Part1.soil_analysis;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,20 +9,37 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.wia2007_zerohunger.Part1.MainActivityP1;
 import com.example.wia2007_zerohunger.R;
+import com.example.wia2007_zerohunger.databinding.ActivityMainSoilBinding;
 
 public class MainSoilActivity extends AppCompatActivity {
+
+    ActivityMainSoilBinding mainSoilBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main_soil);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        mainSoilBinding = ActivityMainSoilBinding.inflate(getLayoutInflater());
+        setContentView(mainSoilBinding.getRoot());
+
+        mainSoilBinding.soilP1Toolbar.setNavigationOnClickListener(v -> {
+            Intent intent = new Intent(MainSoilActivity.this, MainActivityP1.class);
+            startActivity(intent);
         });
+
+        mainSoilBinding.soilAnalysisButtonP1.setOnClickListener(v -> {
+            Intent intent = new Intent(MainSoilActivity.this, SoilAnalysisActivity.class);
+            startActivity(intent);
+        });
+
+        mainSoilBinding.cropRecommentButtonP1.setOnClickListener(v -> {
+            Intent intent = new Intent(MainSoilActivity.this, CropRecommendationActivity.class);
+            startActivity(intent);
+        });
+
     }
 
     //haha
