e247a170df1cf15b6684b817b3ed9ad4914cc13c
OIZHENHAU <zhenhau8072@gmail.com>
2024-12-18 23:34:11 +0800

    Update API Key

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,5 +75,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.squareup.picasso:picasso:2.8")
+    implementation("com.google.android.gms:play-services-location:21.3.0");
+    implementation("com.google.android.gms:play-services-maps:19.0.0");
 
 }
