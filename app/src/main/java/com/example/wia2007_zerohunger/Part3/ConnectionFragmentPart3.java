a0b07a45923a30cf8692d9ef33851a188d367484
OIZHENHAU <zhenhau8072@gmail.com>
2024-12-28 10:20:35 +0800

    Add Part 3

---
+++ app/src/main/java/com/example/wia2007_zerohunger/Part3/ConnectionFragmentPart3.java
@@ -0,0 +1,22 @@
+package com.example.wia2007_zerohunger.Part3;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.wia2007_zerohunger.R;
+
+
+public class ConnectionFragmentPart3 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_connection_part3, container, false);
+    }
+}
