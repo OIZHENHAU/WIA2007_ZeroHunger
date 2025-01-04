2043fd4459d33386f158eff0d87d4ba6d3246ccb
OIZHENHAU <zhenhau8072@gmail.com>
2025-01-04 15:31:01 +0800

    Update Email Sent

--- app/src/main/java/com/example/wia2007_zerohunger/SignUp.java
+++ app/src/main/java/com/example/wia2007_zerohunger/SignUp.java
@@ -31,9 +31,40 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import java.io.File;
+import java.time.LocalDate;
+import java.util.Base64;
+
+
+import java.io.IOException;
+
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
+import android.text.Editable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextWatcher;
+import android.text.style.ForegroundColorSpan;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class SignUp extends AppCompatActivity {
 
@@ -91,6 +122,11 @@ public class SignUp extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
+                            try {
+                                sendEmail(userEmail, "Welcome to Zero Hunger", "Please verify your account");
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
 
                             Intent intent = new Intent(SignUp.this, MainMenu.class);
                             intent.putExtra("nickName", userName);
@@ -113,5 +149,44 @@ public class SignUp extends AppCompatActivity {
         Matcher matcher = pattern.matcher(password);
         return matcher.matches();
     }
+
+    public void sendEmail(String email, String title, String content)  throws IOException {
+        String json = "{\"recipient\": \""+email+"\",\"subject\": \""+title+"\",\"message\": \""+content+"\"}";
+
+        // OkHttp client
+        OkHttpClient client = new OkHttpClient();
+
+        // Request body with JSON data
+        RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), json);
+
+        // Build request
+        Request request = new Request.Builder()
+                .url("https://nodemaillin.netlify.app/.netlify/functions/sendEmail")
+                .post(body)
+                .build();
+
+        // Make the asynchronous call
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Email sent successfully! Please check your Email", Toast.LENGTH_SHORT).show();
+                    System.out.println("Email sent successfully!");
+
+                } else {
+                    System.out.println("Failed to send email. Response code: " + response.code());
+                }
+
+
+            }
+        });
+
+    }
+
 
 }
